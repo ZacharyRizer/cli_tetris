7ba26bf3aaf4a24a6ca7da782633e7df5bca9f98
Zachary Rizer <zacharyrizer@gmail.com>
2020-10-02 12:35:18 -0400

    removed 'using namespace sf' and add bin directory for cmake binary placement

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <SFML/Graphics.hpp>
 #include <time.h>
-using namespace sf;
 
 const int M = 20;
 const int N = 10;
@@ -34,46 +33,43 @@ bool check() {
 int main() {
   srand(time(0));
 
-  RenderWindow window(VideoMode(320, 480), "The Game!");
+  sf::RenderWindow window(sf::VideoMode(320, 480), "The Game!");
 
-  Texture t1, t2, t3;
-  t1.loadFromFile(
-      "/Users/proton/Coding/cpp_projects/cli_tetris/images/tiles.png");
-  t2.loadFromFile(
-      "/Users/proton/Coding/cpp_projects/cli_tetris/images/background.png");
-  t3.loadFromFile(
-      "/Users/proton/Coding/cpp_projects/cli_tetris/images/frame.png");
+  sf::Texture t1, t2, t3;
+  t1.loadFromFile("images/tiles.png");
+  t2.loadFromFile("images/background.png");
+  t3.loadFromFile("images/frame.png");
 
-  Sprite s(t1), background(t2), frame(t3);
+  sf::Sprite s(t1), background(t2), frame(t3);
 
   int dx = 0;
   bool rotate = 0;
   int colorNum = 1;
   float timer = 0, delay = 0.3;
 
-  Clock clock;
+  sf::Clock clock;
 
   while (window.isOpen()) {
     float time = clock.getElapsedTime().asSeconds();
     clock.restart();
     timer += time;
 
-    Event e;
+    sf::Event e;
     while (window.pollEvent(e)) {
-      if (e.type == Event::Closed)
+      if (e.type == sf::Event::Closed)
         window.close();
 
-      if (e.type == Event::KeyPressed) {
-        if (e.key.code == Keyboard::Up)
+      if (e.type == sf::Event::KeyPressed) {
+        if (e.key.code == sf::Keyboard::Up)
           rotate = true;
-        else if (e.key.code == Keyboard::Left)
+        else if (e.key.code == sf::Keyboard::Left)
           dx = -1;
-        else if (e.key.code == Keyboard::Right)
+        else if (e.key.code == sf::Keyboard::Right)
           dx = 1;
       }
     }
 
-    if (Keyboard::isKeyPressed(Keyboard::Down))
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
       delay = 0.05;
 
     //// <- Move -> ///
@@ -139,21 +135,21 @@ int main() {
     delay = 0.3;
 
     /////////draw//////////
-    window.clear(Color::White);
+    window.clear(sf::Color::White);
     window.draw(background);
 
     for (int i = 0; i < M; i++)
       for (int j = 0; j < N; j++) {
         if (field[i][j] == 0)
           continue;
-        s.setTextureRect(IntRect(field[i][j] * 18, 0, 18, 18));
+        s.setTextureRect(sf::IntRect(field[i][j] * 18, 0, 18, 18));
         s.setPosition(j * 18, i * 18);
         s.move(28, 31); // offset
         window.draw(s);
       }
 
     for (int i = 0; i < 4; i++) {
-      s.setTextureRect(IntRect(colorNum * 18, 0, 18, 18));
+      s.setTextureRect(sf::IntRect(colorNum * 18, 0, 18, 18));
       s.setPosition(a[i].x * 18, a[i].y * 18);
       s.move(28, 31); // offset
       window.draw(s);
